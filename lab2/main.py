d18ff8c39bb9edca76bc00b44cbe52336636ea8c
mklimek <klimekmiko@gmail.com>
2023-03-14 21:38:49 +0100

    Allow any origin update

--- lab2/main.py
+++ lab2/main.py
@@ -8,15 +8,10 @@ import json
 app = FastAPI()
 client = httpx.AsyncClient()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:5500"
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
